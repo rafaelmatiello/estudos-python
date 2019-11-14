e4c3566e338c52e4ff38a91f721899c6037300ec
Rafael Matiello <rafael.matiello@senior.com.br>
2019-11-14 13:07:43 -0300

    - teste de orientação a oo

--- car.py
+++ car.py
@@ -6,12 +6,6 @@ class Car:
         self.year = 2019
         self.color = 'blue'
 
-   ##def __init__(self,name, make, year, color ):
-   ##     self.make = make
-   ##     self.name = name
-   ##     self.year = year
-   ##     self.color = color
-
     def drive(self):
         print(self.name + " started")
 
