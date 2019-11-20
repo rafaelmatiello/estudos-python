94f337342b732c56868e89de89b773b96f113736
Rafael Matiello <rafael.matiello@gmail.com>
2019-11-19 21:23:21 -0300

    Teste linux
    Testando criação de classe no linux

---
+++ calculator/calculadora.py
@@ -0,0 +1,11 @@
+class Calculadora(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def sum(self, x, y):
+        return x + y
+
+    def sub(self, x, y ):
+        return x-y
+    
---
+++ testCalculadora.py
@@ -0,0 +1,8 @@
+from calculator.calculadora import Calculadora
+
+calc = Calculadora("test")
+print(calc.name)
+
+print(calc.sum(1, 2))
+
+print(calc.sub(1, 2))
